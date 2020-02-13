356701ff016eac6872e816f37ae3702e8f20060a
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-02-13 11:34:01 +0000

    added start button functionality

--- src/App.js
+++ src/App.js
@@ -29,22 +29,51 @@ class App extends React.Component {
         <header>
           <h1>Hangman Game</h1>
         </header>
-        <button>Start Game</button>
+        <button onClick={this.startGame}>Start Game</button>
         <Hangman
           incorrectGuesses={this.state.incorrectGuesses}
           hasLost={this.hasLost}
+        />
+        <Word
+          guessedLetter={this.state.guessedLetter}
+          splitWord={this.state.splitWord}
+          guessedWord={this.state.guessedWord}
+          hasWon={this.hasWon}
         />
       </main>
     );
   }
 
+  startGame = () => {
+    const randomIndex = Math.floor(Math.random() * 10);
+    console.log(randomIndex);
+    this.setState(currentState => {
+      const selectedWord = currentState.wordOptions[randomIndex];
+      const splitWord = selectedWord.split('');
+      const guessedWord = selectedWord.replace(/\w/g, '_').split('');
+      return {
+        selectedWord,
+        splitWord,
+        guessedWord,
+        incorrectGuesses: 0
+      };
+    });
+  };
+
   hasLost = () => {};
+
+  hasWon = () => {};
 }
 
 function Hangman(props) {
-  return(
-    <p>Number of incorrect guesses: {props.incorrectGuesses}</p>
-  )
+  return <p>Number of incorrect guesses: {props.incorrectGuesses}</p>;
+}
+
+function Word(props) {
+  const newGuessedWord = props.guessedWord.map(letter => {
+    return letter + ' ';
+  });
+  return <p>{newGuessedWord}</p>;
 }
 
 export default App;
